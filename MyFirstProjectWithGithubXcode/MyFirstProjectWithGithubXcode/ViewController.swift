248f17274db307082cbc4a6d0ed4c84d0ec79f0d
Sourabh Kumbhar <sourabh.s.kumbhar@gmail.com>
2021-05-31 11:27:49 +0530

    My First commit

---
+++ MyFirstProjectWithGithubXcode/MyFirstProjectWithGithubXcode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFirstProjectWithGithubXcode
+//
+//  Created by Sourabh Kumbhar on 31/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+        print("My First Xcode Github Project")
+    }
+
+
+}
+
